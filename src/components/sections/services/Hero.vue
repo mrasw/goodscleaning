960c7df9e88154d1f83c5e4b8a85146744975304
tux0112 <bayusb313@gmail.com>
2025-12-20 16:05:54 +0700

    adjust Services\Hero

--- src/components/sections/services/Hero.vue
+++ src/components/sections/services/Hero.vue
@@ -10,8 +10,10 @@ const props = defineProps({
 <template>
     <section>
         <div class="
-            bg-linear-to-b from-hijau-text to-hijau-1 space-y-8 pt-13 pb-11
-        ">
+                bg-linear-to-b from-hijau-text to-hijau-1 space-y-8 pt-13 pb-11
+            "
+            :class="[props.app.isDesktopDevice?'':'px-3']"
+            >
             <div class="text-center">
                 <article class="prose text-white font-poppins-500 mx-auto px-5"
                 :class="[props.app.isDesktopDevice?'text-6xl w-2/3':'text-2xl']"

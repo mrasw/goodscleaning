42d662589e77215ad8ec7587a3cb29a28068ed1f
tux0112 <bayusb313@gmail.com>
2026-01-11 14:00:14 +0700

    ini image about us page

--- src/components/sections/home/body/AboutUs.vue
+++ src/components/sections/home/body/AboutUs.vue
@@ -1,22 +1,12 @@
 <script setup>
-// import AboutUs01 from "../../../../assets/images/home/aboutus/AboutUs01.webp"
-// import AboutUs02 from "../../../../assets/images/home/aboutus/AboutUs02.webp"
-// import AboutUs03 from "../../../../assets/images/home/aboutus/AboutUs03.webp"
-// import Toko from "../../../../assets/images/home/aboutus/Toko Goods Cleaning Oktober 2025.webp"
-import AboutUs01 from "../../../../assets/images/home/aboutus/AboutUs01.jpeg"
-import AboutUs02 from "../../../../assets/images/home/aboutus/AboutUs02.jpeg"
-// import AboutUs03 from "../../../../assets/images/home/aboutus/AboutUs03.jpeg"
-import AboutUs03 from "../../../../assets/images/home/aboutus/AboutUs03.webp"
-import Toko from "../../../../assets/images/home/aboutus/Toko Goods Cleaning Oktober 2025.jpeg"
-import OurServices01 from "../../../../assets/images/home/aboutus/OurServices01.webp"
+    import AboutUs01 from "../../../../assets/images/home/aboutus/AboutUs01.webp"
+    import AboutUs02 from "../../../../assets/images/home/aboutus/AboutUs02.webp"
+    import AboutUs03 from "../../../../assets/images/home/aboutus/AboutUs03.webp"
+    import Toko from "../../../../assets/images/home/aboutus/Toko Goods Cleaning Oktober 2025.webp"
 
-
-import { ref, onMounted, computed } from 'vue';
-
-
-const props = defineProps({
-    app: Object,
-})
+    const props = defineProps({
+        app: Object,
+    })
 
 </script>
 
@@ -77,7 +67,7 @@ const props = defineProps({
                     <!-- Left: items end -->
                     <div class="grid grid-flow-row justify-items-end gap-2 md:gap-4 pb-13 w-1/2">
                         <div class="bg-gray-200 w-2/3 aspect-square rounded-3xl">
-                            <!-- <img :src="AboutUs01" alt="" class="object-cover w-full h-full rounded-3xl" loading="lazy" decoding="async"> -->
+                            <img :src="AboutUs01" alt="" class="object-cover w-full h-full rounded-3xl" loading="lazy" decoding="async">
                         </div>
                         <div class="bg-gray-200 w-full aspect-square rounded-3xl">
                             <img :src="AboutUs03" alt="" class="object-cover w-full h-full rounded-3xl" loading="lazy" decoding="async">
@@ -87,11 +77,10 @@ const props = defineProps({
                     <!-- Right: items start -->
                     <div class="grid grid-flow-row justify-items-start gap-2 md:gap-4 pt-13 w-1/2">
                         <div class="bg-gray-200 w-full aspect-square rounded-3xl">
-                            <!-- <img :src="Toko" alt="" class="object-cover w-full h-full rounded-3xl" loading="lazy" decoding="async"> -->
-                            <img :src="OurServices01" alt="" class="object-cover w-full h-full rounded-3xl" loading="lazy" decoding="async">
+                            <img :src="Toko" alt="" class="object-cover w-full h-full rounded-3xl" loading="lazy" decoding="async">
                         </div>
                         <div class="bg-gray-200 w-2/3 aspect-square rounded-3xl">
-                            <!-- <img :src="AboutUs02" alt="" class="object-cover w-full h-full rounded-3xl" loading="lazy" decoding="async"> -->
+                            <img :src="AboutUs02" alt="" class="object-cover w-full h-full rounded-3xl" loading="lazy" decoding="async">
                         </div>
                     </div>
                 </div>
@@ -100,49 +89,5 @@ const props = defineProps({
         </div>
 
     </section>
-
-    <!-- <section v-else-if="app.isMobileDevice">
-        <div class="px-3">
-            <div>
-                <div>
-                    <span
-                        class="font-poppins text-base text-hijau-text underline underline-offset-8 decoration-hijau-decoration">
-                        ABOUT US
-                    </span>
-                </div>
-                <div class="mt-10">
-                    <div>
-                        <span class="text-5xl font-poppins-600 text-hijau-text">
-                            Bringing Clean, Comfort, and Care Together
-                        </span>
-                    </div>
-                    <div class="mt-6">
-                        <span class="text-base font-poppins-300 text-hijau-text">
-                            We are a team of passionate cleaning experts who take pride in delivering the
-                            highest standard of service. With years of experience in the industry, we’ve
-                            perfected our cleaning methods to ensure every job is done right.
-                        </span>
-                    </div>
-                </div>
-                <div class="mt-6" :class="[]">
-                    <button
-                        class="btn text-kuning bg-hijau-text dark:bg-kuning dark:text-hijau-text border-none shadow-none  font-poppins font-bold">
-                        Learn More
-                    </button>
-                </div>
-            </div>
-            <div class="flex gap-2 mt-8 justify-center">
-                <div class="grid grid-flow-row justify-items-end gap-2 pb-13">
-                    <div class="bg-gray-200 w-[62%] aspect-square rounded-3xl"></div>
-                    <div class="bg-gray-200 size-40 aspect-square rounded-3xl"></div>
-                </div>
-                <div class="grid grid-flow-row justify-items-start gap-2 pt-13">
-                    <div class="bg-gray-200 size-40 rounded-3xl"></div>
-                    <div class="bg-gray-200 w-[62%] aspect-square rounded-3xl"></div>
-                </div>
-            </div>
-        </div>
-    </section> -->
-    <!-- end of about us -->
 
 </template>
